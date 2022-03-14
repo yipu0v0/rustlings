5a22a0b560dba8de6ef87ae95017caf698a9c7d4
yipu0v0 <yipu0v0@qq.com>
2022-03-14 13:40:59 +0800

    Revert "merge"
    This reverts commit 72a86b7b3b8d45816b49353cfc0c6e6e85181f17.

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -3,20 +3,18 @@
 // 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
-
 mod delicious_snacks {
 
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    use self::fruits::PEAR as PEAR;
+    use self::veggies::CUCUMBER as APPLE;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -25,7 +23,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        delicious_snacks::fruits::APPLE,
+        delicious_snacks::veggies::CARROT
     );
 }
